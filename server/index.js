699af06a42bca29b5798a069f65c9ea89736f07f
SoyaChunkz <sameermaroof26@gmail.com>
2024-09-03 19:30:42 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://dsr-three.vercel.app/",
+    origin: "https://dsr-three.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
